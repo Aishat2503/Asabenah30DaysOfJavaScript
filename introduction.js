2a9cc994dba601b1a2dbde60fcefa93a881da7e6
Aishat2503 <ibrahimaishat2503@gmail.com>
2024-01-17 12:35:36 +0100

    DayOne

---
+++ introduction.js
@@ -0,0 +1,14 @@
+console.log('Welcome to 30DaysOfJavaScript');
+let firstName = 'Asabenah'
+let lastName = 'Yeteyeh'
+let country = 'Finland'
+let city = 'Helsinki'
+let age = 100
+let isMarried = true
+const gravity = 9.81
+const boilingPoint = 100
+const PI = 3.14
+let name = 'Asabenah', job = 'teacher', live = 'Finland'
+console.log(firstName , lastName , country , city ,age , isMarried) ;
+console.log(gravity, boilingPoint, PI);
+console.log(name, job, live);
