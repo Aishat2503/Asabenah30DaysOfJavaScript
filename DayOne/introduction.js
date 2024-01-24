ef5c29e0461c9645d47b6f3812e2e94ad9be248c
Aishat2503 <ibrahimaishat2503@gmail.com>
2024-01-24 12:22:15 +0100

    Day One

---
+++ DayOne/introduction.js
@@ -0,0 +1,5 @@
+ //comment can make code readable
+ 
+   //Welcome to 30DaysOfJavaScript
+   /*comment can make code readable
+   easy to reuse and informative */
---
+++ DayOne/variable.js
@@ -0,0 +1,36 @@
+let firstName = 'Abake'
+let isMarried = false
+let healthStatus
+let emptyValue = null
+console.log(firstName)
+console.log(isMarried)
+console.log(healthStatus)
+console.log(emptyValue)
+console.log(typeof 'Aisha')
+console.log(typeof false)
+console.log(typeof healthStatus)
+console.log(typeof null)
+let lastName
+let city_rove
+let year2024
+let $50
+let middleName ='Adebusola'
+let lastMonth ='December'
+let cityRove ='Newspaper'
+let blind = false
+let first_Name ='Aisha'
+let last_Name ='Ibrahim'
+let maritalStatus ='Single'
+let country ='Nigeria'
+let age = 25
+console.log(first_Name, last_Name, maritalStatus, country, age)
+let first_name ='Aishat', last_name ='Ibraheem', maritalstatus ='single', couNtry ='Ghana', aGe ='23'
+console.log(first_name)
+console.log(last_name)
+console.log(maritalstatus)
+console.log(couNtry) 
+console.log(aGe)
+let myAge =('I am 15 years old') 
+let yourAge =('You are 30 years old')
+console.log(myAge)
+console.log(yourAge)
