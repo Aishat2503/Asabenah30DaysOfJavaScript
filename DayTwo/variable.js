e89bddc618ef049c7fe04125b66ffc62c4754a10
Aishat2503 <ibrahimaishat2503@gmail.com>
2024-01-28 21:10:54 +0100

    cont

---
+++ DayTwo/variable.js
@@ -0,0 +1,79 @@
+// Level 1
+let challenge = "  30 Days of JavaScript  "
+console.log(challenge)
+console.log(challenge.length)
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+console.log(challenge.substr(0, 1))
+console.log(challenge.includes('Script'))
+console.log(challenge.split())
+console.log(challenge.split(' '))
+console.log(challenge.repeat(2))
+let string = ('Facebook Google Microsoft Apple IBM Oracle Amazon')
+console.log(string)
+console.log(string.split(' '))
+console.log(challenge.replace('JavaScript', 'Python'))
+console.log(challenge.charCodeAt('J'))
+console.log(challenge.indexOf('a'))
+console.log(challenge.lastIndexOf('a'))
+const sentence = "You can end a sentence with because because because is a conjuction"
+console.log(sentence)
+console.log(sentence.indexOf('because'))
+console.log(sentence.lastIndexOf('because'))
+console.log(sentence.search('because'))
+console.log(challenge.trim(''))
+console.log(challenge.startsWith(" "))
+console.log(challenge.startsWith('30'))
+console.log(challenge.match('a'))
+let java = ("30DaysOf")
+console.log(java)
+console.log(java.concat("JavaScript"))
+
+// Level 2
+let quote = "There is no exercise better for the heart than reaching down and lifting people up"
+console.log(quote)
+let quote2 = "Love is not patronizing and charity isn't about pity, it is about love. Charity and Love are the same"
+console.log(quote2)
+let num = "10" === 10
+console.log(num)
+let digit = '10'
+let digitInt = parseInt(digit)
+console.log(digitInt)
+console.log(typeof(num))
+console.log(typeof 9.8, 10)
+let parseFloat = 9.8
+console.log(parseFloat)
+console.log(Math.round(9.8))
+let text = 'python and jargon'
+console.log(text)
+console.log(text.includes('on'))
+let sen = "i hope this course is not full of Jargon"
+console.log(sen)
+console.log(sen.includes('Jargon'))
+// console.log(Math.random() * 100)
+// console.log(Math.random() * 101)
+// console.log(Math.random() * 256) 
+// let jav = "JavaScript"
+// console.log(jav.indexOf('JavaScript'))
+console.log('1\t1\t1\t1\t1')
+console.log('2\t1\t2\t4\t8')
+console.log('3\t1\t3\t9\t27')
+console.log('4\t1\t4\t16\t64')
+console.log('5\t1\t5\t25\t125')
+console.log()
+let sent = "You can end a sentence with because because because is a conjuction"
+console.log(sent)
+console.log(sent.substr(28, 23))
+
+// Level 3
+let love = "Love is the best thing in this world. Some found their love and some are still looking for their love"
+let patTern = /love/gi
+console.log(love.match(patTern))
+let pattern = /because/gi
+console.log(sent.match(pattern))
+
+
+
+
+
+
